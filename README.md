ed2aef8552712bd3f870ec28e5601e5273721875
yuk1ty <yuki.mul.tiplus@gmail.com>
2021-09-11 16:36:30 +0900

    最初のコミット

---
+++ README.md
@@ -0,0 +1,34 @@
+# sabi
+
+In Japanese version https://github.com/bnjbvr/rouille. Shamelessly copied and updated from it.
+
+日本語で Rust プログラムを書くことができます！
+
+# 例
+
+`main.rs`
+
+```rust
+sabi::sabi! {
+    外 クレート sabi;
+
+    関数 メイン() {
+        書き出す!("こんにちは、世界");
+    }
+}
+```
+
+出力結果。
+
+```
+❯ cargo run
+こんにちは、世界
+```
+
+# Thank you
+
+https://github.com/bnjbvr/rouille
+
+# License
+
+[WTFPL](http://www.wtfpl.net/)
---
+++ sabi_proc_macro/src/lib.rs
@@ -0,0 +1,108 @@
+// shamelessly copied from https://github.com/bnjbvr/rouille/blob/principale/rouille_proc_macro/src/lib.rs
+// Thank you to the authors!
+
+use proc_macro::{Group, Ident, TokenStream, TokenTree};
+
+fn replace_ident(ident: Ident) -> Option<TokenTree> {
+    let ident_str = ident.to_string();
+
+    let new_str = match ident_str.as_str() {
+        "ダメ" => "Err",
+        "よし" => "Ok",
+        "文字列" => "String",
+        "辞書" => "HashMap",
+        "デフォルト" => "Default",
+        "エラー" => "Error",
+        "オプション" => "Option",
+        "存在" => "Some",
+        "なし" => "None",
+        "結果" => "Result",
+        "セルフ" => "Self",
+        "書き出す" => "println",
+        "止める" => "break",
+        "非同期" => "async",
+        "待つ" => "await",
+        "ループ" => "loop",
+        "ムーブ" => "move",
+        "クレート" => "crate",
+        "として" => "as",
+        "定数" => "const",
+        "マッチ" => "match",
+        "安全じゃない" => "unsafe",
+        "中" => "in",
+        "から" => "from",
+        "動的" => "dyn",
+        "空ける" => "unwrap",
+        "デフォ" => "default",
+        "入出力" => "io",
+        "外" => "extern",
+        "偽" => "false",
+        "関数" => "fn",
+        "親" => "super",
+        "実装" => "impl",
+        "挿入" => "insert",
+        "束" => "trait",
+        "取得" => "get",
+        "モジュール" => "mod",
+        "可変" => "mut",
+        "新" => "new",
+        "条件" => "where",
+        "繰り返し" => "for",
+        "取得または挿入" => "get_or_insert_with",
+        "メイン" => "main",
+        "公開" => "pub",
+        "ない？" | "ない?" => None?,
+        "返す" => "return",
+        "もし" => "if",
+        "でないならば" => "else",
+        "自身" => "self",
+        "束縛" => "let",
+        "静的" => "static",
+        "構造体" => "struct",
+        "期待する" => "expect",
+        "間" => "while",
+        "使う" => "use",
+        "真" => "true",
+        "列挙" => "enum",
+
+        _ => &ident_str,
+    };
+
+    let new_ident = Ident::new(new_str, ident.span());
+    Some(TokenTree::Ident(new_ident))
+}
+
+fn replace_tree(tok: TokenTree, out: &mut Vec<TokenTree>) {
+    match tok {
+        TokenTree::Group(group) => {
+            let mut group_elem = Vec::new();
+            replace_stream(group.stream(), &mut group_elem);
+            let mut new_stream = TokenStream::new();
+            new_stream.extend(group_elem);
+            out.push(TokenTree::Group(Group::new(group.delimiter(), new_stream)));
+        }
+        TokenTree::Ident(ident) => {
+            if let Some(ident) = replace_ident(ident) {
+                out.push(ident);
+            }
+        }
+        TokenTree::Punct(..) | TokenTree::Literal(..) => {
+            out.push(tok);
+        }
+    }
+}
+
+fn replace_stream(ts: TokenStream, out: &mut Vec<TokenTree>) {
+    for tok in ts {
+        replace_tree(tok, out)
+    }
+}
+
+#[proc_macro]
+pub fn sabi(item: TokenStream) -> TokenStream {
+    let mut returned = Vec::new();
+    replace_stream(item, &mut returned);
+    let mut out = TokenStream::new();
+    out.extend(returned);
+    out
+}
